51d33c858753fb23824436b44286a2bc11f82f4d
Jonas-Grestenberger <jonas.grestenberger@htlwy.at>
2024-12-03 20:23:51 +0100

    Kontaktformular Auswertung überarbeitet

--- backend.js
+++ backend.js
@@ -7,6 +7,7 @@ const path = require('path');
 const url = 'https://api.openai.com/v1/chat/completions';
 const express = require('express');
 const { send } = require('process');
+const { time } = require('console');
 const app = express();
 let chat = [];
 let stringForOutputChat = "";
@@ -106,14 +107,74 @@ function handleError(error) {
     console.error(error);
 }
 //#endregion
-function handleContactFormular(req,res){
-    let input = req.url + "";
 
-    let Name = input.split("name=")[1].split("&")[0];
-    let Email = input.split("email=")[1].split("&")[0];
-    let Betreff = input.split("betreff=")[1].split("&")[0];
-    let Message = input.split("message=")[1].split("&")[0];
-    let text=fs.readFileSync('ContactFormular.txt','utf8');
-    fs.writeFileSync('ContactFormular.txt',text+ '\nBetreff: ' + Betreff +  '\nName: ' + Name + '\nEmail: ' + Email + '\nMessage: ' + Message + '\n'+'\n\n');
-  res.end();
+let errorHelper = 1;
+function handleContactFormular(req,res){
+
+  let input = req.url + "";
+  
+    if((errorHelper%7 == 0 || errorHelper == 1)&& input != "/KontaktSeite.css" && input != "/favicon.ico"){
+      
+      input = decode(input);
+      let now = new Date();
+      
+      let Name = input.split("name=")[1].split("&")[0];
+      let Email = input.split("email=")[1].split("&")[0];
+      let Betreff = input.split("betreff=")[1].split("&")[0];
+      let Message = input.split("message=")[1].split("&")[0];
+      let text=fs.readFileSync('ContactFormular.txt','utf8');
+      fs.writeFileSync('ContactFormular.txt', text +'\nValue/ID: '+now.getHours()+":"+now.getMinutes()+" | "+now.getDate()+"/"+now.getMonth()+"/"+now.getFullYear() + '\nBetreff: ' + Betreff +  '\nName: ' + Name + '\nEmail: ' + Email + '\nMessage: ' + Message + '\n'+'\n\n');
+      errorHelper = errorHelper+1;
+      res.write(fs.readFileSync('KontaktSeite.html','utf8'));
+      res.write('<style>'+fs.readFileSync('KontaktSeite.css','utf8')+'</style>');
+      res.write('<style>'+fs.readFileSync('HeaderFooter.css','utf8')+'</style>');
+      res.end()
+      
+    }
+    else{
+      
+      errorHelper = errorHelper+1;
+      res.write(fs.readFileSync('KontaktSeite.html','utf8'));
+      res.write('<style>'+fs.readFileSync('KontaktSeite.css','utf8')+'</style>');
+      res.write('<style>'+fs.readFileSync('HeaderFooter.css','utf8')+'</style>');
+      res.end();
+  }
+}
+function decode(inputQuery){
+
+   for(let i = 0; i < inputQuery.length; i++) {
+    inputQuery = inputQuery.replace("%0D%0A", "\n");
+    inputQuery = inputQuery.replace("+", " ");
+    inputQuery = inputQuery.replace("%3B", ";");
+    inputQuery = inputQuery.replace("%C3%9C", "Ü");
+    inputQuery = inputQuery.replace("%C3%84", "Ä");
+    inputQuery = inputQuery.replace("%C3%96", "Ö");
+    inputQuery = inputQuery.replace("%C3%BC", "ü");
+    inputQuery = inputQuery.replace("%C3%A4", "ä");
+    inputQuery = inputQuery.replace("%C3%B6", "ö");
+    inputQuery = inputQuery.replace("%C3%9F", "ß");
+    inputQuery = inputQuery.replace("%E1%BA%9E", "ẞ");
+    inputQuery = inputQuery.replace("%3A", ":");
+    inputQuery = inputQuery.replace("%2C", ",");
+    inputQuery = inputQuery.replace("%23", "#");
+    inputQuery = inputQuery.replace("%27", "'");
+    inputQuery = inputQuery.replace("%7E", "~");
+    inputQuery = inputQuery.replace("%28", "(");
+    inputQuery = inputQuery.replace("%29", ")");
+    inputQuery = inputQuery.replace("%5B", "[");
+    inputQuery = inputQuery.replace("%5D", "]");
+    inputQuery = inputQuery.replace("%7B", "{");
+    inputQuery = inputQuery.replace("%7D", "}");
+    inputQuery = inputQuery.replace("%3E", ">");
+    inputQuery = inputQuery.replace("%3C", "<");
+    inputQuery = inputQuery.replace("%2F", "/");
+    inputQuery = inputQuery.replace("%3F", "?");
+    inputQuery = inputQuery.replace("%21", "!");
+    inputQuery = inputQuery.replace("%2D", "-");
+    inputQuery = inputQuery.replace("%5E", "^");
+    inputQuery = inputQuery.replace("%7C", "|");
+    inputQuery = inputQuery.replace("%5C", "\\");
+    inputQuery = inputQuery.replace("%40", "@");
+   }
+    return inputQuery
 }
